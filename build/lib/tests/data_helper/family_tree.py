96f0d402624f17a0d7bd10622c69d7c6a919630f
Akanksha Priyadarshini <17mar.akanksha@gmail.com>
2020-10-16 15:47:55 +0530

    dir clean up

--- build/lib/tests/data_helper/family_tree.py
+++
@@ -1,7 +0,0 @@
-from src.models import family_tree as family_tree_model
-
-
-def create_family_tree(members, head_member):
-    family_tree = family_tree_model.FamilyTree(members)
-    family_tree.set_head_member(head_member)
-    return family_tree
--- src/action/file.py
+++ src/action/file.py
@@ -4,14 +4,17 @@ from src.validation import is_empty_user_input
 from src.action import member as member_action
 
 
+import re
+
+
 def execute_file_input(file_path, family_tree_instance):
     input_data_file = open(file_path, "r")
     data_file_lines = input_data_file.readlines()
     for line in data_file_lines:
-        if is_empty_user_input(line.rstrip()):
+        if is_empty_user_input(line.strip()):
             error_msg = output_messages['INVALID_INPUT']
             print(error_msg)
-
-        input_command = line.rstrip().split(' ')
+        input_string = re.sub(' +', ' ', line.strip())
+        input_command = input_string.split(' ')
         action = input_command[0]
         member_action.execute(action, family_tree_instance, input_command[1::])
